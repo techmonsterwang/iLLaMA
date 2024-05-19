d3dd0cf80091bb50e71329379e71ba0e565f6414
techmonsterwang <2959688260@qq.com>
2024-05-20 03:16:53 +0800

    first commit

--- scripts/eval_illama_tiny_in1k.sh
+++ scripts/eval_illama_tiny_in1k.sh
@@ -1,11 +1,46 @@
 root_imagenet='/mnt/petrelfs/wangjiahao/datasets/classificaton/imagenet/'
+
+
+# illama-tiny: 75.0
+MODEL=illama_tiny
 RESUME='/mnt/petrelfs/wangjiahao/DoiT/pretrained/illama-tiny-in1k-75.0.pth'
 
-python main.py --model illama_tiny --eval true \
---resume /path/to/model \
---data_path $root_imagenet
+srun -p gvembodied \
+    --job-name=eval \
+    --gres=gpu:1 \
+    --cpus-per-task=16 \
+    --preempt \
+    --quotatype=spot \
+    python main.py --model $MODEL --eval true \
+    --resume $RESUME \
+    --data_path $root_imagenet
 
 
+# illama-small: 79.9
+MODEL=illama_small
+RESUME='/mnt/petrelfs/wangjiahao/DoiT/pretrained/illama-small-in1k-79.9.pth'
+
+srun -p gvembodied \
+    --job-name=eval \
+    --gres=gpu:1 \
+    --cpus-per-task=16 \
+    --preempt \
+    --quotatype=spot \
+    python main.py --model $MODEL --eval true \
+    --resume $RESUME \
+    --data_path $root_imagenet
 
 
+# illama-base: 81.6
+MODEL=illama_base
+RESUME='/mnt/petrelfs/wangjiahao/DoiT/pretrained/illama-base-in1k-81.6.pth'
 
+srun -p gvembodied \
+    --job-name=eval \
+    --gres=gpu:1 \
+    --cpus-per-task=16 \
+    --preempt \
+    --quotatype=spot \
+    python main.py --model $MODEL --eval true \
+    --resume $RESUME \
+    --data_path $root_imagenet
