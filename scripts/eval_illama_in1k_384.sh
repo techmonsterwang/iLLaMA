ca69f26461e172003fa13297da9c75410bb36b37
techmonsterwang <2959688260@qq.com>
2024-05-20 04:13:55 +0800

    first commit

--- scripts/eval_illama_in1k_384.sh
+++ scripts/eval_illama_in1k_384.sh
@@ -17,3 +17,50 @@ srun -p gvembodied \
     --data_path $root_imagenet \
     --resume $RESUME
 
+
+# illama-base: 85.0
+MODEL=illama_base
+RESUME='/mnt/petrelfs/wangjiahao/DoiT/pretrained/illama-base-in21kin1k-384-85.0.pth'
+
+srun -p gvembodied \
+    --job-name=evaluation_384 \
+    --gres=gpu:2 \
+    --cpus-per-task=32 \
+    --preempt \
+    --quotatype=reserved \
+    python -m torch.distributed.launch --nproc_per_node=2 main_soft_fthr.py \
+    --model $MODEL --input_size 384 --eval true \
+    --data_path $root_imagenet \
+    --resume $RESUME
+
+
+# illama-large: 84.8
+MODEL=illama_large
+RESUME='/mnt/petrelfs/wangjiahao/DoiT/pretrained/illama-large-in21kin1k-224-84.8.pth'
+
+srun -p gvembodied \
+    --job-name=evaluation_384 \
+    --gres=gpu:2 \
+    --cpus-per-task=32 \
+    --preempt \
+    --quotatype=reserved \
+    python -m torch.distributed.launch --nproc_per_node=2 main_soft_fthr.py \
+    --model $MODEL --input_size 384 --eval true \
+    --data_path $root_imagenet \
+    --resume $RESUME
+
+
+# illama-large: 86.0
+MODEL=illama_large
+RESUME='/mnt/petrelfs/wangjiahao/DoiT/pretrained/illama-large-in21kin1k-384-86.0.pth'
+
+srun -p gvembodied \
+    --job-name=evaluation_384 \
+    --gres=gpu:2 \
+    --cpus-per-task=32 \
+    --preempt \
+    --quotatype=reserved \
+    python -m torch.distributed.launch --nproc_per_node=2 main_soft_fthr.py \
+    --model $MODEL --input_size 384 --eval true \
+    --data_path $root_imagenet \
+    --resume $RESUME
