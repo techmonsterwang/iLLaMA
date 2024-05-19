abc674bd87fc6a2cfd2741ba02bb7e71e6024d3e
techmonsterwang <2959688260@qq.com>
2024-05-20 03:41:53 +0800

    first commit

--- scripts/eval_illama_in1k_224.sh
+++ scripts/eval_illama_in1k_224.sh
@@ -48,3 +48,19 @@ srun -p gvembodied \
     --model $MODEL --eval true \
     --data_path $root_imagenet \
     --resume $RESUME
+
+
+# illama-base: 83.6
+MODEL=illama_base
+RESUME='/mnt/petrelfs/wangjiahao/DoiT/pretrained/illama-base-in21kin1k-224-83.6.pth'
+
+srun -p gvembodied \
+    --job-name=evaluation_224 \
+    --gres=gpu:2 \
+    --cpus-per-task=32 \
+    --preempt \
+    --quotatype=spot \
+    python -m torch.distributed.launch --nproc_per_node=2 main.py \
+    --model $MODEL --eval true \
+    --data_path $root_imagenet \
+    --resume $RESUME
