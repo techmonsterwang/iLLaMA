3c1c24830e1d6cf9ab7c625e9afd0fcf4d51e379
techmonsterwang <2959688260@qq.com>
2024-05-21 02:05:09 +0800

    first commit

--- main_soft_fthr.py
+++ main_soft_fthr.py
@@ -1,10 +1,3 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-
-# All rights reserved.
-
-# This source code is licensed under the license found in the
-# LICENSE file in the root directory of this source tree.
-
 import argparse
 import datetime
 import numpy as np
